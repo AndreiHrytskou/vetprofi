ed2f9be3508ed27fa3b4f22d4b513eefbeae849d
Андрей Грицков <andrejgrickov@MacBook-Air-Andrej.local>
2025-06-11 21:31:33 +0200

    first commit

---
+++ partials/header.php
@@ -0,0 +1,39 @@
+<header id="header" class="header">
+  <div class="container">
+    <div class="header__block">
+      <img src="/Applications/MAMP/htdocs/vetprofi_php/assets/images/VetProfi.svg" alt="logo" />
+      <div class="social__block">
+        <a href="" target="_blank">
+          <img src="" alt="" />
+        </a>
+        <a href="" target="_blank">
+          <img src="" alt="" />
+        </a>
+        <a href="" target="_blank">
+          <img src="" alt="" />
+        </a>
+      </div>
+    </div>
+    <nav class="menu">
+      <ul class="menu__list">
+        <li class="menu__item"></li>
+        <li class="menu__item"></li>
+        <li class="menu__item"></li>
+      </ul>
+    </nav>
+    <div class="header__block">
+      <div class="hedaer__search"></div>
+      <div class="lang__wrapper">
+        <ul class="langflags">
+          <li class="lang">RU</li>
+          <li class="lang">EN</li>
+        </ul>
+      </div>
+    </div>
+    <div class="login__block">
+      <!-- Кнопки -->
+      <button class="auth-trigger" data-tab="login">Войти</button>
+      <button class="auth-trigger" data-tab="register">Регистрация</button>
+    </div>
+  </div>
+</header>
