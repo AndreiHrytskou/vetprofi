7a65e049c09fe5cde4c0c4e7ef3017752a5d1d11
andrejgrickov <andreihrytskou@gmail.com>
2025-06-23 22:33:55 +0200

    done page vacancies

---
+++ vacancies.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>VetProfi</title>
+  </head>
+  <body>
+	<?php include "./partials/header.php"?>
+	<main class="main">
+		<div class="container">
+			<?php include "./partials//module/breadcrumbs.php"?>
+			<div class="vacancies__wrapper">
+				<h1 class="title">Вакансии</h1>
+				<div class="vacancies__list">
+					<div class="vacancies__item">
+						<div class="vacancies__item-header">
+							<h2 class="subtitle">Редактор / Вет-контент-менеджер</h2>
+							<h3 class="title-h3">от 30 000 до 60 000 ₽</h3>
+							<p class="text">(в зависимости от занятости, опыта и количества задач)</p>
+						</div>
+						<div class="vacancies__item-description">
+							<h3 class="title-h3">Обязанности :</h3>
+							<ul class="btn__list">
+								<li class="text">Подготовка и редактирование материалов по ветеринарной тематике (уход, болезни, питание и поведение животных)</li>
+								<li class="text">Проверка фактов, терминов и источников информации</li>
+								<li class="text">Сотрудничество с ветеринарными экспертами, фриланс-авторами и дизайнерами</li>
+								<li class="text">Адаптация материалов под структуру сайта и SEO</li>
+								<li class="text">Курирование публикаций и обновление устаревших статей</li>
+							</ul>
+						</div>
+						<div class="vacancies__item-description">
+							<h3 class="title-h3">Требования :</h3>
+							<ul class="btn__list">
+								<li class="text">Опыт работы редактором, контент-менеджером или автором (от 1 года)</li>
+								<li class="text">Знание основ ветеринарии, терминологии и тематики ухода за животными</li>
+								<li class="text">Грамотная письменная речь, умение писать и редактировать «внятно и по делу»</li>
+								<li class="text">Навыки работы в Google Docs, Trello/Notion — желательно</li>
+								<li class="text">Ответственность, внимательность к деталям, самостоятельность</li>
+							</ul>
+						</div>
+						<div class="vacancies__item-description">
+							<h3 class="title-h3">Условия :</h3>
+							<ul class="btn__list">
+								<li class="text">Удалённая работа, гибкий график (но с дедлайнами)</li>
+								<li class="text">Своевременные выплаты, дружная команда</li>
+								<li class="text">Возможность развиваться внутри проекта (рост до редактора раздела или куратора авторов)</li>
+								<li class="text">Доступ к закрытым экспертным материалам и обучению внутри команды</li>
+							</ul>
+						</div>
+					</div>
+					<div class="vacancies__item">
+						<div class="vacancies__item-header">
+							<h2 class="subtitle">Редактор / Вет-контент-менеджер</h2>
+							<h3 class="title-h3">от 30 000 до 60 000 ₽</h3>
+							<p class="text">(в зависимости от занятости, опыта и количества задач)</p>
+						</div>
+						<div class="vacancies__item-description">
+							<h3 class="title-h3">Обязанности :</h3>
+							<ul class="btn__list">
+								<li class="text">Подготовка и редактирование материалов по ветеринарной тематике (уход, болезни, питание и поведение животных)</li>
+								<li class="text">Проверка фактов, терминов и источников информации</li>
+								<li class="text">Сотрудничество с ветеринарными экспертами, фриланс-авторами и дизайнерами</li>
+								<li class="text">Адаптация материалов под структуру сайта и SEO</li>
+								<li class="text">Курирование публикаций и обновление устаревших статей</li>
+							</ul>
+						</div>
+						<div class="vacancies__item-description">
+							<h3 class="title-h3">Требования :</h3>
+							<ul class="btn__list">
+								<li class="text">Опыт работы редактором, контент-менеджером или автором (от 1 года)</li>
+								<li class="text">Знание основ ветеринарии, терминологии и тематики ухода за животными</li>
+								<li class="text">Грамотная письменная речь, умение писать и редактировать «внятно и по делу»</li>
+								<li class="text">Навыки работы в Google Docs, Trello/Notion — желательно</li>
+								<li class="text">Ответственность, внимательность к деталям, самостоятельность</li>
+							</ul>
+						</div>
+						<div class="vacancies__item-description">
+							<h3 class="title-h3">Условия :</h3>
+							<ul class="btn__list">
+								<li class="text">Удалённая работа, гибкий график (но с дедлайнами)</li>
+								<li class="text">Своевременные выплаты, дружная команда</li>
+								<li class="text">Возможность развиваться внутри проекта (рост до редактора раздела или куратора авторов)</li>
+								<li class="text">Доступ к закрытым экспертным материалам и обучению внутри команды</li>
+							</ul>
+						</div>
+					</div>
+					<div class="vacancies__item">
+						<div class="vacancies__item-header">
+							<h2 class="subtitle">Редактор / Вет-контент-менеджер</h2>
+							<h3 class="title-h3">от 30 000 до 60 000 ₽</h3>
+							<p class="text">(в зависимости от занятости, опыта и количества задач)</p>
+						</div>
+						<div class="vacancies__item-description">
+							<h3 class="title-h3">Обязанности :</h3>
+							<ul class="btn__list">
+								<li class="text">Подготовка и редактирование материалов по ветеринарной тематике (уход, болезни, питание и поведение животных)</li>
+								<li class="text">Проверка фактов, терминов и источников информации</li>
+								<li class="text">Сотрудничество с ветеринарными экспертами, фриланс-авторами и дизайнерами</li>
+								<li class="text">Адаптация материалов под структуру сайта и SEO</li>
+								<li class="text">Курирование публикаций и обновление устаревших статей</li>
+							</ul>
+						</div>
+						<div class="vacancies__item-description">
+							<h3 class="title-h3">Требования :</h3>
+							<ul class="btn__list">
+								<li class="text">Опыт работы редактором, контент-менеджером или автором (от 1 года)</li>
+								<li class="text">Знание основ ветеринарии, терминологии и тематики ухода за животными</li>
+								<li class="text">Грамотная письменная речь, умение писать и редактировать «внятно и по делу»</li>
+								<li class="text">Навыки работы в Google Docs, Trello/Notion — желательно</li>
+								<li class="text">Ответственность, внимательность к деталям, самостоятельность</li>
+							</ul>
+						</div>
+						<div class="vacancies__item-description">
+							<h3 class="title-h3">Условия :</h3>
+							<ul class="btn__list">
+								<li class="text">Удалённая работа, гибкий график (но с дедлайнами)</li>
+								<li class="text">Своевременные выплаты, дружная команда</li>
+								<li class="text">Возможность развиваться внутри проекта (рост до редактора раздела или куратора авторов)</li>
+								<li class="text">Доступ к закрытым экспертным материалам и обучению внутри команды</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+				<div class="vacancies__form">
+					<h2 class="subtitle">Обратная связь</h2>
+					<div class="vacancies__form-container">
+						<form class="vacancies__form-block">
+							<input type="text" required placeholder="ФИО *">
+							<input type="email" name="" id="" required placeholder="Ваш email *">
+							<textarea name="" id="" cols="30" rows="10" required style="resize: none;" placeholder="Комментарий *"></textarea>
+							<div class="input-checkbox">
+								<label for="personalData">
+								<input type="checkbox" name="personalData" id="personalData" required>
+								<span class="checkmark"></span>Соглашение на обработку <a href="">персональных данных</a></label>
+							</div>
+							<input class="btn" type="button" value="Отправить">
+						</form>
+						<img src="http://localhost:8888/vetprofi_php/assets/images/dog.png" alt="dog" class="vacancies__form-image">
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+	<?php include "./partials/footer.php"?>
+	<div class="modal hidden">
+		<?php include "./partials/modal.php"?>
+	</div>
+  </body>
+</html>
